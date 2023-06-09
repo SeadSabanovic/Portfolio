ae7e14584b4dbeb080bc9c6b452f960287e23f59
Sead Sabanovic <s.sabanovic@agentur-loop.com>
2023-06-10 00:39:30 +0200

    Mouse Follow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
-import {
-  Component,
-  OnInit,
-  AfterViewInit,
-} from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { fade } from './animations/fade';
 import { routerFade } from './animations/router';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-root',
@@ -17,13 +14,24 @@ export class AppComponent implements OnInit, AfterViewInit {
   scroll: any;
   isLoading = false;
 
-  constructor() {
-    // setTimeout(() => (this.isLoading = false), 3000);
-  }
-
   ngOnInit(): void {}
 
-  ngAfterViewInit(): void {}
+  ngAfterViewInit(): void {
+    const mouse = document.querySelector('.mouse'),
+      duration = 0.6,
+      xTo = gsap.quickTo(mouse, 'x', { duration: duration, ease: 'power3' }),
+      yTo = gsap.quickTo(mouse, 'y', { duration: duration, ease: 'power3' });
+
+    gsap.set(mouse, { xPercent: -50, yPercent: -50 });
+    gsap.to(mouse, {
+      opacity: 1,
+      duration: 2,
+    });
+    window.addEventListener('mousemove', (e: MouseEvent) => {
+      xTo(e.x);
+      yTo(e.y);
+    });
+  }
 
   getAnimationData(outlet: RouterOutlet) {
     return outlet.isActivated ? outlet.activatedRoute : '';
